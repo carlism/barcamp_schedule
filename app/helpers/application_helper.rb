f1dd26da711091d4abe2d94225daf67aa1fb25b5
Carl Leiby <carl.leiby@gmail.com>
2010-04-22 12:56:45 -0400

    Adding some formatting starting on the rooms page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def set_focus(id)
+    javascript_tag("$('#{id}').focus()")
+  end
 end
